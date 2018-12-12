555e254fb700d3d38e6e05fb7576739d2746ab58
mengting <44832490+mengtingchu@users.noreply.github.com>
2018-12-12 09:45:36 +0100

    Update detect_faces.py

--- detect_faces.py
+++ detect_faces.py
@@ -39,39 +39,32 @@ image = cv2.imread(args["image"])
 blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0,
 	(300, 300), (104.0, 177.0, 123.0))
 
-# pass the blob through the network and obtain the detections and predictions
 print("[INFO] computing object detections...")
-# set the blob to be the input of the AI
 net.setInput(blob)
-# run the AI, get all detected faces and put the data in a list called detections
 detections = net.forward()
 
 people_counter = 0
 # loop over the detected faces (contained in the list 'detections')
 for i in range(0, detections.shape[2]):
-	# extract the 
 	(i.e., probability) associated with the prediction
 	confidence = detections[0, 0, i, 2]
 
-	# loop the detection that has a confidence than the set one
+	# loop the detection that has a confidence > condifence threshold
 	if confidence > args["confidence"]:
 		people_counter += 1
 
-		# compute the (x, y)-coordinates of the bounding box for the
-		# object
+		# compute the (x, y)-coordinates to add the square
 		box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
 		# startX, startY endX endY are the coordinates
 		(startX, startY, endX, endY) = box.astype("int")
 
-		# draw the bounding box of the face along with the associated
-		# probability
+		# probability of being a human face 
 		text = "{:.2f}%".format(confidence * 100)
 		y = startY - 10 if startY - 10 > 10 else startY + 10
 
-		# this line draz the bounding box
+		
 		cv2.rectangle(image, (startX, startY), (endX, endY),
 			(0, 0, 255), 2)
-		# This line zrite the confidence text on the image
 		cv2.putText(image, text, (startX, y),
 			cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
 
